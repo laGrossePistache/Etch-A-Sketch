1e223bdf868360a784d0d1de60d559426c2f71bb
Tommy Melançon-Roy <tmelanconroy@gmail.com>
2022-08-11 15:46:49 -0400

    Started to code javascript.js, added grid of square in the flex-container with for loop.. and add hover effect!

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const flexContainer = document.querySelector(".flex-container");
+
+for (let index = 0; index < 256; index++) {
+    const gridElement = document.createElement('div');
+    gridElement.classList.add('grid-element');
+    
+    flexContainer.appendChild(gridElement);
+       
+}
+
+function getClickGridElement(){
+    
+}
+
+
