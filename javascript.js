8859bc4a4541187778f4eb21124303840a0b9ba6
Tommy Melançon-Roy <tmelanconroy@gmail.com>
2022-08-12 13:39:41 -0400

    Add slider with text to display the grid size. Also added better UI

--- javascript.js
+++ javascript.js
@@ -1,33 +1,41 @@
 const flexContainer = document.querySelector(".flex-container");
+const gridSlider = document.querySelector("#grid-range");
+const gridSizeElement = document.querySelector("#grid-size");
 const gridElements = [];
 
-generateNewGrid(1);
+generateNewGrid(gridSlider.value);
 
-function generateNewGrid(gridSize){
-    const finalElementSize = 1024 / gridSize;
-    const finalGridSize = 1024 * 1024;
-    const finalGridElementNumber = finalGridSize / (finalElementSize*finalElementSize);
-    console.log(finalElementSize);
-    for (let index = 0; index < finalGridElementNumber; index++) {
-            const gridElement = document.createElement('div');
-            gridElement.classList.add('grid-element');
-            gridElement.addEventListener("click", (e) => getClickGridElement(e));
-            gridElement.setAttribute('style', `width: ${finalElementSize}px; height: ${finalElementSize}px`);
-            gridElements.push(gridElement);
-            
-            flexContainer.appendChild(gridElement);
-                       }
-        
-        console.log(gridElements.length);      
-    }
+gridSlider.addEventListener("change", (e) => {
+  generateNewGrid(e.target.value);
+  gridSizeElement.textContent = `${gridSlider.value}x${gridSlider.value}`;
+});
 
-function clearGridElements(){
-    gridElements.forEach(element => {
-        element.remove();
-    });
+function generateNewGrid(gridSize) {
+  clearGridElements();
+  const finalElementSize = 1024 / gridSize;
+  const finalGridSize = 1024 * 1024;
+  const finalGridElementNumber =
+    finalGridSize / (finalElementSize * finalElementSize);
+  for (let index = 0; index < finalGridElementNumber; index++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("grid-element");
+    gridElement.addEventListener("click", (e) => getClickGridElement(e));
+    gridElement.setAttribute(
+      "style",
+      `width: ${finalElementSize}px; height: ${finalElementSize}px`
+    );
+    gridElements.push(gridElement);
+
+    flexContainer.appendChild(gridElement);
+  }
 }
 
-function getClickGridElement(e){
-    e.target.style.background = "black";
+function clearGridElements() {
+  gridElements.forEach((element) => {
+    element.remove();
+  });
 }
 
+function getClickGridElement(e) {
+  if (gridElements.length > 0) e.target.style.background = "black";
+}
