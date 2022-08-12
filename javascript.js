75ecee4a93153469cb53ea5466466dbe237bc83d
Tommy Melançon-Roy <tmelanconroy@gmail.com>
2022-08-12 10:20:36 -0400

    You can now change de grid size and it will fit inside de game 'cavas' without problem. Had to play a bit with math to make it possible.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,33 @@
 const flexContainer = document.querySelector(".flex-container");
+const gridElements = [];
 
-for (let index = 0; index < 256; index++) {
-    const gridElement = document.createElement('div');
-    gridElement.classList.add('grid-element');
-    
-    flexContainer.appendChild(gridElement);
-       
+generateNewGrid(1);
+
+function generateNewGrid(gridSize){
+    const finalElementSize = 1024 / gridSize;
+    const finalGridSize = 1024 * 1024;
+    const finalGridElementNumber = finalGridSize / (finalElementSize*finalElementSize);
+    console.log(finalElementSize);
+    for (let index = 0; index < finalGridElementNumber; index++) {
+            const gridElement = document.createElement('div');
+            gridElement.classList.add('grid-element');
+            gridElement.addEventListener("click", (e) => getClickGridElement(e));
+            gridElement.setAttribute('style', `width: ${finalElementSize}px; height: ${finalElementSize}px`);
+            gridElements.push(gridElement);
+            
+            flexContainer.appendChild(gridElement);
+                       }
+        
+        console.log(gridElements.length);      
+    }
+
+function clearGridElements(){
+    gridElements.forEach(element => {
+        element.remove();
+    });
 }
 
-function getClickGridElement(){
-    
+function getClickGridElement(e){
+    e.target.style.background = "black";
 }
-
 
